973ce982227bdf9d76bb365d45d9295feaca5a0d
mgkim <alsrms27@gmail.com>
2021-03-06 20:44:35 +0900

    add dictionary layout && set app color

---
+++ app/src/main/java/com/voca_application/dictionary/Dictionary.java
@@ -0,0 +1,16 @@
+package com.voca_application.dictionary;
+
+import android.os.Bundle;
+
+import com.voca_application.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Dictionary extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dictionary);
+    }
+
+}
