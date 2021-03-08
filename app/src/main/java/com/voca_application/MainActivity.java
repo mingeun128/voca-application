9d97edca1bb0a06a1a3466e09b54c60574883ed4
mgkim <alsrms27@gmail.com>
2021-03-08 22:10:08 +0900

    MU YA~ HO~

--- app/src/main/java/com/voca_application/MainActivity.java
+++ app/src/main/java/com/voca_application/MainActivity.java
@@ -6,17 +6,23 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 import com.voca_application.dictionary.DictionaryFragment;
+import com.voca_application.voca_exam.VocaExamFragment;
 import com.voca_application.voca_note.VocaNoteFragment;
 
 public class MainActivity extends AppCompatActivity {
     private FrameLayout DictionaryLayout;
     private FragmentManager fragmentManager;
     private FragmentTransaction transaction;
+    Button DictionaryButton;
+    Button VocaNoteButton;
+    Button VocaExamButton;
     DictionaryFragment Dictionary;
     VocaNoteFragment VocaNote;
+    VocaExamFragment VocaExam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +31,47 @@ public class MainActivity extends AppCompatActivity {
 
         Dictionary = new DictionaryFragment();
         VocaNote = new VocaNoteFragment();
+        VocaExam = new VocaExamFragment();
 
         fragmentManager = getSupportFragmentManager();
         transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.frameLayout, Dictionary).commitAllowingStateLoss();
+        transaction.replace(R.id.frameLayout, Dictionary);
+        transaction.commit();
+
+        DictionaryButton = findViewById(R.id.btnDict);
+        VocaNoteButton = findViewById(R.id.btnVocaNote);
+        VocaExamButton = findViewById(R.id.btnVocaExam);
+
+        buttonHandler();
 
     }
 
-    private void buttonHandler(View view){
-        transaction = fragmentManager.beginTransaction();
-        switch(view.getId())
-        {
-            case R.id.btnDict:
-                transaction.replace(R.id.frameLayout, Dictionary).commitAllowingStateLoss();
-                break;
-            case R.id.btnVocaNote:
-                transaction.replace(R.id.frameLayout, VocaNote).commitAllowingStateLoss();
-                break;
-        }
+    private void buttonHandler(){
+        DictionaryButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.frameLayout, Dictionary);
+                transaction.commit();
+            }
+        });
+
+        VocaNoteButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.frameLayout, VocaNote);
+                transaction.commit();
+            }
+        });
+
+        VocaExamButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.frameLayout, VocaExam);
+                transaction.commit();
+            }
+        });
     }
 }
